a837d61c9729d1ac6bd8e8f25e02b48ba4b26bbb
Dz <zn.dantez@gmail.com>
2021-10-13 09:50:35 +0300

    Проект с примером обобщенний

---
+++ C2L3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C2L3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
