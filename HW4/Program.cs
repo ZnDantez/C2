b3af1559a647840e27372770997d339e4fef18aa
Dz <zn.dantez@gmail.com>
2021-10-18 14:27:40 +0300

    Пытаюсь поменять ключи и значения в словаре

---
+++ HW4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW4
+{
+    class Program
+    {
+        // 2.Дана коллекция List<T>, требуется подсчитать,
+        // сколько раз каждый элемент встречается в данной коллекции:
+        // а) для целых чисел;
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 3, 4, 5, 6, 7, 6, 5, 4, 3, 4, 5, 6, 5 };
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+
+            foreach (var i in list)
+            {
+                foreach (var j in pairs)
+                {
+                    if (i != j.Key)
+                    {
+                        j.Key = i;
+                    }
+
+                    if (i == j.Key)
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
